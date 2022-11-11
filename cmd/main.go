4de903fbf873315d2916da65bbefb4a1b72fbe37
Viktorydogger <valcevav@gmail.com>
2022-11-11 17:24:53 +0300

    4

--- cmd/main.go
+++ cmd/main.go
@@ -1,5 +1,14 @@
 package main
 
+import (
+	avitotestapp "forgo/AvitoTest-app"
+	"log"
+)
+
 func main() {
+	srv := new(avitotestapp.Server)
+	if err := srv.Run("0000"); err != nil {
+		log.Fatalf("error occured while running http server: %s", err.Error())
+	}
 
 }
